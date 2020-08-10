a1d68d775f76054a6356e9e29196f8141106f95c
iHeroGH <jojomedhat2004@gmail.com>
2020-08-10 16:49:36 -0700

    Fixed prefix stufffxz

--- cogs/PrefixCommands.py
+++ cogs/PrefixCommands.py
@@ -13,6 +13,9 @@ class PrefixCommands(commands.Cog, name = "Prefix Commands"):
     @commands.command()
     async def prefix(self, ctx, prefix = None):
         """Changes the prefix of the server (or gives you set prefix)"""
+        
+        if ctx.guild is None:
+            await ctx.send("This command does not work in DMs. Use the command on a server you have the `manage server` permission on.")
 
         if prefix is None:
             connection = await asyncpg.connect(**self.bot.database_auth)
@@ -20,16 +23,23 @@ class PrefixCommands(commands.Cog, name = "Prefix Commands"):
             await connection.close()
             await ctx.send(f"The prefix for this server is `{prefixRows[0]['prefix']}`")
             return
-            
 
-        if ctx.author.guild_permissions.manage_guild:
-            if len(prefix) > 0 and len(prefix) <= 50:
-                connection = await asyncpg.connect(**self.bot.database_auth)
-                await connection.fetch("INSERT into prefix (guildid, prefix) VALUES ($1, $2) on conflict (guildid) do update set prefix = $2", ctx.guild.id, prefix)
-                await connection.close()
-                await ctx.send(f"Set prefix to `{prefix}`")
-            if len(prefix) > 50:
-                await ctx.send("Could not set that as a prefix as it is longer than 50 characters.")
+        if len(prefix) > 50:
+            await ctx.send("Could not set that as a prefix as it is longer than 50 characters.")
+            return
+        if len(prefix) < 1:
+            await ctx.send("Could not set that as a prefix as it is shorter than 1 character.")
+            return
+
+        if ctx.author.guild_permissions.manage_guild is False:
+            await ctx.send("You don't have permission to run this command (You need the `manage server` permission)")
+            return
+
+        connection = await asyncpg.connect(**self.bot.database_auth)
+        await connection.fetch("INSERT into prefix (guildid, prefix) VALUES ($1, $2) on conflict (guildid) do update set prefix = $2", ctx.guild.id, prefix)
+        await connection.close()
+        await ctx.send(f"Set prefix to `{prefix}`")
+
         
 
 def setup(bot):
