f3fe9780d71edd7747a9b10521d8e343a8f735fc
iHeroGH <jojomedhat2004@gmail.com>
2020-08-14 16:07:16 -0700

    commented it out

--- cogs/FilterCommands.py
+++ cogs/FilterCommands.py
@@ -36,28 +36,28 @@ class FilterCommands(commands.Cog, name = "Filter Commands"):
                 await connection.fetch("INSERT INTO filters (userid, channelfilter) VALUES($1, $2);", ctx.author.id, filter)
                 await connection.close()
 
-            if filterType == acceptableFilterTypes[2]:
+            # if filterType == acceptableFilterTypes[2]:
 
-                connection = await asyncpg.connect(**self.bot.database_auth)
-                rows = await connection.fetch("select * from filters where userid = $1;", ctx.author.id)
-                await connection.close()
+            #     connection = await asyncpg.connect(**self.bot.database_auth)
+            #     rows = await connection.fetch("select * from filters where userid = $1;", ctx.author.id)
+            #     await connection.close()
 
-                if len(rows) == 0:
-                    await ctx.send(f"You don't have any keywords. Set some up by running the `{ctx.prefix}addkeyword` command")
-                    return
+            #     if len(rows) == 0:
+            #         await ctx.send(f"You don't have any keywords. Set some up by running the `{ctx.prefix}addkeyword` command")
+            #         return
 
-                textFilters = []
-                channelFilters = []
+            #     textFilters = []
+            #     channelFilters = []
                 
-                for row in rows:
-                    if row != None:
-                        textFilters.append(row['textfilter'])
-                        channelFilters.append(row['channelfilter'])
+            #     for row in rows:
+            #         if row != None:
+            #             textFilters.append(row['textfilter'])
+            #             channelFilters.append(row['channelfilter'])
 
-                textFilters = ', '.join(textFilters)
-                channelFilters = ', '.join(channelFilters)
+            #     textFilters = ', '.join(textFilters)
+            #     channelFilters = ', '.join(channelFilters)
 
-                await ctx.send(f"Text Filters: `{[textFilters]}` \n Channel Filters: `{channelFilters}`")
+            #     await ctx.send(f"Text Filters: `{[textFilters]}` \n Channel Filters: `{channelFilters}`")
 
         else:
             await ctx.send(f"You didn't provide an acceptable filter type (`{ctx.prefix} filter (text, channnel, list) (filter)`)")
