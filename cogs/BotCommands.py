8771dbeee8a99ed2c110ef8114f95856c0e37a14
iHeroGH <jojomedhat2004@gmail.com>
2020-08-15 04:40:59 -0700

    fixed the debugging things

--- cogs/BotCommands.py
+++ cogs/BotCommands.py
@@ -200,11 +200,11 @@ class BotCommands(commands.Cog, name="Bot Commands"):
                 channelFilters = await db("SELECT * FROM channelfilters WHERE userid=$1", member.id)
                 serverFilters = await db("SELECT * FROM serverfilters WHERE userid=$1", member.id)
 
-            print(i['channelfilter'])
             for i in textFilters:
                 if i['textfilter'] == message.content:
                     content = ""
             for i in channelFilters:
+                print(['channelfilter'])
                 if i['channelfilter'] == message.channel:
                     print("Log 2")
                     content = ""
