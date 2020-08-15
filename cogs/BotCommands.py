e6e6273f1866886e1da5b722245accb99ac2e0ca
iHeroGH <jojomedhat2004@gmail.com>
2020-08-15 05:18:15 -0700

    removed debug

--- cogs/BotCommands.py
+++ cogs/BotCommands.py
@@ -207,9 +207,7 @@ class BotCommands(commands.Cog, name="Bot Commands"):
                 if i['channelfilter'] == message.channel.id:
                     content = ""
             for i in serverFilters:
-                print(f"{i['serverfilter']} extra debug")
                 if message.guild.id == i['serverfilter']:
-                    print(message.guild.id)
                     content = ""
 
             if content == "":
