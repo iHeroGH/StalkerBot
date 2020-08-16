c4b50e69882562a57ce5d9967445193ae7f3859b
iHeroGH <jojomedhat2004@gmail.com>
2020-08-15 17:38:26 -0700

    Added guild joining logging

---
+++ cogs/LoggerAndHandler.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+#from discord import Webhook, AsyncWebhookAdapter
+
+import aiohttp
+import asyncpg
+
+
+class LoggerAndHandler(commands.Cog, name="Logger And Handler"):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+
+        async with aiohttp.ClientSession() as session:
+            webhook = discord.Webhook.from_url('https://discordapp.com/api/webhooks/744353242322043001/V3WMdShI8L8LZLStNUBaqG2WI-qZrdofCQFM1QkW4oLTIcRA4TMC5ffKFpS2JyIXp96w', adapter=discord.AsyncWebhookAdapter(session))
+            await webhook.send(f'StalkerBot was added to {guild.name} ({guild.id})', username='On Guild Join Event')
+
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(LoggerAndHandler(bot))
