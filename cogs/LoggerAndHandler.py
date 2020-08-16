2bb9628968d0d22c31fd41194e2a668d22018c5b
iHeroGH <jojomedhat2004@gmail.com>
2020-08-15 18:42:35 -0700

    changed error to a string

--- cogs/LoggerAndHandler.py
+++ cogs/LoggerAndHandler.py
@@ -36,7 +36,7 @@ class LoggerAndHandler(commands.Cog, name="Logger And Handler"):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
 
-        error.read().decode('utf-8')
+        error = str(error)
         data = io.StringIO(error)
         data.seek(0)
         async with aiohttp.ClientSession() as session:
