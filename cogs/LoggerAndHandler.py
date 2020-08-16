d2b5a332246e703288d0119cad61fe4364b35e8c
iHeroGH <jojomedhat2004@gmail.com>
2020-08-15 17:41:50 -0700

    Added remove event log and member count

--- cogs/LoggerAndHandler.py
+++ cogs/LoggerAndHandler.py
@@ -16,10 +16,15 @@ class LoggerAndHandler(commands.Cog, name="Logger And Handler"):
 
         async with aiohttp.ClientSession() as session:
             webhook = discord.Webhook.from_url('https://discordapp.com/api/webhooks/744353242322043001/V3WMdShI8L8LZLStNUBaqG2WI-qZrdofCQFM1QkW4oLTIcRA4TMC5ffKFpS2JyIXp96w', adapter=discord.AsyncWebhookAdapter(session))
-            await webhook.send(f'StalkerBot was added to {guild.name} ({guild.id})', username='On Guild Join Event')
+            await webhook.send(f'StalkerBot was added to `{guild.name}` (`{guild.id}`). `{len(guild.members)}` members.', username='On Guild Join Event')
 
 
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
 
+        async with aiohttp.ClientSession() as session:
+            webhook = discord.Webhook.from_url('https://discordapp.com/api/webhooks/744353242322043001/V3WMdShI8L8LZLStNUBaqG2WI-qZrdofCQFM1QkW4oLTIcRA4TMC5ffKFpS2JyIXp96w', adapter=discord.AsyncWebhookAdapter(session))
+            await webhook.send(f'StalkerBot was removed from `{guild.name}` (`{guild.id}`). `{len(guild.members)}` members.', username='On Guild Leave Event')
 
 
 
