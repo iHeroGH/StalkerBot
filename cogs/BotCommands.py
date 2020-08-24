60f133f9406f2dcb83f0835f7156f70c18abdd32
iHeroGH <jojomedhat2004@gmail.com>
2020-08-24 09:12:40 -0700

    -tias idk

--- cogs/BotCommands.py
+++ cogs/BotCommands.py
@@ -220,8 +220,8 @@ class BotCommands(commands.Cog, name="Bot Commands"):
             for i in settingDict[member.id]['filters']['textfilters']:
                 if i.lower() in message.content.lower() and content is not None:
                     lowercaseI = i.lower()
-                    content.lower()
-                    content = content.replace(lowercaseI, "")
+                    lowerContent = content.lower()
+                    content = lowerContent.replace(lowercaseI, "")
 
             # If there's no content to be examined, let's just skip the message
             if content is None or content.strip() == "":
