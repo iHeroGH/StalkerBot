fa8e2edbd97feb559e659ccc8264ea1bd4bede6d
iHeroGH <jojomedhat2004@gmail.com>
2020-08-27 17:41:39 -0700

    Added hero-stalking to memgan

--- cogs/BotCommands.py
+++ cogs/BotCommands.py
@@ -130,7 +130,7 @@ class BotCommands(commands.Cog, name="Bot Commands"):
         # Stalk people list
         userID = {
             141231597155385344: [megan, sapnap],
-            539508943592882193: []
+            322542134546661388: [megan]
         }
 
         # Filter out bots
