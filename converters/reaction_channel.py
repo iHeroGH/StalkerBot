3acdac2657bf29595897311263cef048e89dc8bd
George Matta <jojomedhat2004@gmail.com>
2020-11-30 20:23:11 -0800

    returned an int in the reaction channel converter

--- converters/reaction_channel.py
+++ converters/reaction_channel.py
@@ -9,7 +9,7 @@ class ReactionChannel(commands.Converter):
 
         # This function accepts any ints, and only returns a value if the int matches the format of a channel ID
         if re.match(r"^([0-9]){16,24}$", value):
-            return value
+            return int(value)
 
         # It didn't convert and return an argument, so we've gotta raise an error
         raise commands.BadArgument("`{0}` isn't a valid ID.".format(value))
