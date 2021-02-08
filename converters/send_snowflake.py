b06db9196b0fc909a314f462432ec4e58c5194f8
George Matta <jojomedhat2004@gmail.com>
2021-02-07 22:38:56 -0800

    string?

--- converters/send_snowflake.py
+++ converters/send_snowflake.py
@@ -8,8 +8,8 @@ class SendSnowflake(commands.Converter):
     async def convert(self, ctx, value):
 
         # This function accepts any ints, and only returns a value if the int matches the format of a user or channel ID
-        if re.match(r"^(<@!?)?(?P<ID>[0-9]{16,24})>?$", value):
-            return re.match(r"^(<@!?)?(?P<ID>[0-9]{16,24})>?$", value).group("ID")
+        if re.match(r"^(<!?@|(<#)?)?(?P<ID>[0-9]{16,24})>?$", str(value)):
+            return re.match(r"^(<!?@|(<#)?)?(?P<ID>[0-9]{16,24})>?$", str(value)).group("ID")
 
         # It didn't convert and return an argument, so we've gotta raise an error
         raise commands.BadArgument("`{0}` isn't a valid ID.".format(value))
