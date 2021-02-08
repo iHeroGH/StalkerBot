047f3afe5104d60d0d0625d3f1d95eef20247276
George Matta <jojomedhat2004@gmail.com>
2021-02-07 22:50:07 -0800

    made sure to return an int

--- converters/send_snowflake.py
+++ converters/send_snowflake.py
@@ -9,7 +9,7 @@ class SendSnowflake(commands.Converter):
 
         # This function accepts any ints, and only returns a value if the int matches the format of a user or channel ID
         if re.match(r"^(<@!?|(<#)?)?(?P<ID>[0-9]{16,24})>?$", str(value)):
-            return re.match(r"^(<@!?|(<#)?)?(?P<ID>[0-9]{16,24})>?$", str(value)).group("ID")
+            return int(re.match(r"^(<@!?|(<#)?)?(?P<ID>[0-9]{16,24})>?$", str(value)).group("ID"))
 
         # It didn't convert and return an argument, so we've gotta raise an error
         raise commands.BadArgument("`{0}` isn't a valid ID.".format(value))
