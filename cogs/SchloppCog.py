353bf8c3c7b924d85cc1835f4c5a2d89377699a3
George Matta <jojomedhat2004@gmail.com>
2021-03-14 15:15:22 -0700

    SchloppCog

---
+++ cogs/SchloppCog.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import voxelbotutils as utils
+
+
+class SchloppCog(utils.Cog, name="SchloppShowingHisLove"):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @utils.command(hidden=True)    
+    async def takegeorgeoutfordinner(self, ctx):
+        """Takes george out to dinner"""
+        
+        #Dont want everyone using this.
+        if ctx.author.id != 393305855929483264:
+            return
+        
+        with utils.Embed(use_random_colour=True) as embed:
+            embed.description = "You take george out for a nice, romantic date in a fancy restaurant."
+
+        await ctx.send(embed=embed)
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(SchloppCog(bot))
