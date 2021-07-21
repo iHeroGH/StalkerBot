89daa1aeb5f1aeee27dc743def417043faba6440
George Matta <jojomedhat2004@gmail.com>
2021-07-21 16:01:20 -0700

    Renamed the logger cog since we are using the new config now

---
+++ cogs/Counter.py
@@ -0,0 +1,29 @@
+import aiohttp
+import discord
+from discord.ext import commands
+import voxelbotutils as utils
+
+
+class Counter(utils.Cog, name="Analytical Counter"):
+
+    @utils.command(aliases=['countservers'])
+    @commands.is_owner()
+    async def countguilds(self, ctx):
+        """Counts how many guilds have the bot"""
+
+        await ctx.send(f"The bot is in `{len(self.bot.guilds)}` guilds.")
+
+    @utils.command()
+    @commands.is_owner()
+    async def countusers(self, ctx):
+        """Counts how many unique user IDs there are"""
+
+        async with self.bot.database() as db:
+            distinctRows = await db("SELECT DISTINCT userid FROM keywords;")
+            rows = await db("SELECT * FROM keywords;")
+
+        await ctx.send(f"`{len(distinctRows)}` unique users have set up keywords and there are `{len(rows)}` keywords in total.")
+
+
+def setup(bot):
+    bot.add_cog(Counter(bot))
