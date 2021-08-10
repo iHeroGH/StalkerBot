1e334293317357232109f1a41e4943df61a38036
George Matta <jojomedhat2004@gmail.com>
2021-08-09 18:24:57 -0700

    fix what content to check in stalkingevents

--- cogs/stalking_events.py
+++ cogs/stalking_events.py
@@ -217,7 +217,7 @@ class StalkingEvents(utils.Cog, name="Stalking Events (Message Send/Edit)"):
                     self.bot.logger.debug(f"Not sending message to {user_id} because of user filter")
                     content = None
             for kw in settings_dict[member.id]['filters']['textfilters']:
-                if kw.lower() in content.lower() and content is not None:
+                if kw.lower() in (embed_content or message.content).lower() and content is not None:
                     content = re.sub(re.escape(kw.lower()), "", content.lower())
 
             # If there's no content to be examined, let's just skip the message
