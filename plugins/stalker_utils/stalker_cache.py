a43872dddef8cbc1eb2e4bdf6ba832ebd44e8407
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-08-21 00:28:29 -0700

    start working on caching

---
+++ plugins/stalker_utils/stalker_cache.py
@@ -0,0 +1,12 @@
+import logging
+
+import novus as n
+from novus import types as t
+from novus.utils import Localization as LC
+from novus.ext import client
+
+log = logging.getLogger("plugins.stalker_utils.stalker_cache")
+
+class StalkerCache(client.Plugin):
+
+    ...
